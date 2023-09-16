458d22736fc5636d2adc6e840edb8cfcceb5b626
Andrew Ocampo <aocampo2862@gmail.com>
2023-09-15 23:55:04 -0500

    added starter framework for base dino game

---
+++ Assets/Code/DinoBasic/DinoBasicController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DinoBasic
+{
+    public class DinoBasicController : MonoBehaviour
+    {
+        // Outlet
+        Rigidbody2D _rb;
+
+        // State tracking
+        public int jumpsLeft;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Jump
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                if(jumpsLeft > 0)
+                {
+                    jumpsLeft--;
+                    _rb.AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
+                }
+            }
+        }
+
+        // check for collisions
+        void OnCollisionStay2D(Collision2D other)
+        {
+            // Check that we collided with Ground
+            if(other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            {
+                // Check what is directly below our character's feet
+                RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.down, 1.1f);
+
+                Debug.DrawRay(transform.position, Vector2.down * 1.1f);
+
+                // We might have multiple things below character's feet
+                for(int i = 0; i < hits.Length; i++)
+                {
+                    RaycastHit2D hit = hits[i];
+
+                    // Check that we collided with ground below our feet
+                    if(hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                    {
+                        // Reset jump count
+                        jumpsLeft = 1;
+                    }
+                }
+            }
+        }
+    }
+}
