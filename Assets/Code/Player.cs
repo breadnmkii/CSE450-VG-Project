ade187022cc13b8cab2c403e6751a6945c1945e5
Yonghao Chen <raidenchen@hotmail.com>
2023-09-22 13:57:13 -0500

    Finish 1st version of Obstacles Mechanism
    See details in Assets/Logs/Work_Log_Chen.txt.

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -9,21 +9,20 @@ using UnityEngine;
 // Not involving actuall character behaviors.
 // Todo: figure out whether need to do the rhythm check part in this class or not. Need to discuss detailed control.
 
-public class LaneManager: MonoBehaviour
+public class Player : MonoBehaviour
 {
 
-    // Player Character object.
-    public GameObject Character;
     // A tmp Character to solve possible animation problem.
     public GameObject CharacterShadow;
     // List of lane pivot point, to hold the positions for player to move.
     public List<GameObject> Lanes;
     // duration between shadow and character animation.
     public float duration;
+    // Max HP
+    public int MaxHP;
+    // Health point
+    private int HP;
 
-
-    // Created for rhythm check, currently no use.
-    private int timer;
     // index of lane that the player is on.
     private int lane_No;
 
@@ -31,32 +30,59 @@ public class LaneManager: MonoBehaviour
     private void Start()
     {
         lane_No = 0;
+        HP = MaxHP;
     }
 
     private void Update()
     {
-        timer += 1;
+        if ( this.HP <= 0 )
+        {
+            Destroy(CharacterShadow);
+            Destroy(gameObject);
+        }
         if (Input.GetKeyDown(KeyCode.UpArrow) && lane_No < 3)
         {
             lane_No += 1;
-            Util.Move(Character, Lanes[lane_No], "Lane"+lane_No.ToString());
+            Util.Move(gameObject, Lanes[lane_No], "Lane" + lane_No.ToString());
             StartCoroutine(Util.WaitForSec(duration, () =>
             {
-                Util.Move(CharacterShadow, Character);
+                Util.Move(CharacterShadow, gameObject);
             }));
             Debug.Log(lane_No);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow) && lane_No > 0)
         {
             lane_No -= 1;
-            Util.Move(Character, Lanes[lane_No], "Lane" + lane_No.ToString());
+            Util.Move(gameObject, Lanes[lane_No], "Lane" + lane_No.ToString());
             StartCoroutine(Util.WaitForSec(duration, () =>
             {
-                Util.Move(CharacterShadow, Character);
+                Util.Move(CharacterShadow, gameObject);
             }));
             Debug.Log(lane_No);
         }
     }
 
-    
+
+    // HP Getter
+    public int getHP()
+    {
+        return this.HP;
+    }
+
+
+    // Add current HP by integer n.
+    // n range [-Inf, Inf].
+    // Constraint current HP within range [0, MaxHP] 
+    public void ModifyHP(int n)
+    {
+        this.HP += n;
+        if (HP < 0)
+        {
+            HP = 0;
+        }
+        if (HP > MaxHP)
+        {
+            HP = MaxHP;
+        }
+    }
 }
